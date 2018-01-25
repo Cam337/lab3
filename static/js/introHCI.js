beb67afb6a515ed1e60301ad38d4c21b1524c72c
Cameron <cameronelee42@gmail.com>
2018-01-24 18:36:15 -0800

    added interaction with javascript

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,27 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$(this).text("BOOM");
+		$(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+  $(".project").click(projectClick);
+}
+
+function projectClick(e) {
+	e.preventDefault();
+
+	var containingProject = $(this).closest(".project");
+	var description = $(containingProject).find(".project-description");
+	if(description.length == 0) {
+    containingProject.append("<div class='project-description'><p>Description of Project</p></div>");
+	}
+	else {
+    //description.html("<p>Stop clicking on this! You just did it at " + (new Date()) + "</p>");
+    description.fadeToggle();
+	}
+
+
 }
